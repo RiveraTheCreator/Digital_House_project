3572337735e475053abb8a8102fa776d5f5e4941
Josué <jrgsm70@hotmail.com>
2021-08-12 14:04:38 -0500

    Creación de los archivos html. Hace falta ingresar su contenido

--- src/app.js
+++ src/app.js
@@ -4,9 +4,32 @@ const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
-app.get('/', (req,res)=>{
-    
-})
-app.listen(3000, (req,res)=>{
+
+app.listen(3000, ()=>{
     console.log('Server Arriba');
-})
+});
+
+app.get('/', (req,res)=>{
+	let htmlPath = path.resolve(__dirname,'./views/index.html');
+	res.sendFile(htmlPath);
+});
+
+app.get('/carrito', (req,res)=>{
+	let htmlPath = path.resolve(__dirname,'./views/carrito.html');
+	res.sendFile(htmlPath);
+});
+
+app.get('/detalle', (req,res)=>{
+	let htmlPath = path.resolve(__dirname,'./views/detalle.html');
+	res.sendFile(htmlPath);
+});
+
+app.get('/login', (req,res)=>{
+	let htmlPath = path.resolve(__dirname,'./views/login.html');
+	res.sendFile(htmlPath);
+});
+
+app.get('/registro', (req,res)=>{
+	let htmlPath = path.resolve(__dirname,'./views/registro.html');
+	res.sendFile(htmlPath);
+});
