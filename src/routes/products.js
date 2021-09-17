9e536891afeb525f0df48a606db331495b8705a9
jrgsm70 <jrgsm70@hotmail.com>
2021-09-16 22:23:12 -0500

    Ajuste de media querys en vista detail.ejs

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController.js');
 
+// *** Genera la vista para mostrar todos los productos ***
 router.get('/', productsController.index);
 router.get('/detalle/:id', productsController.detail);
 //Crear producto
 router.get('/crear/', productsController.crear);
 router.post('/',productsController.procesar);
-
+//Obtener un producto... No debes confundir con crear un producto
+router.get('/:id', productsController.detail);
 
 module.exports = router;
